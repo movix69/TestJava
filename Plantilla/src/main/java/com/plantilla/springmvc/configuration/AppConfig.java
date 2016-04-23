5935d760664de748582fdd833adb0b4d13052feb
Oscar <Oscar@192.168.1.33>
2016-04-23 14:12:15 +0200

    Reconfig

--- Plantilla/src/main/java/com/plantilla/springmvc/configuration/AppConfig.java
+++ Plantilla/src/main/java/com/plantilla/springmvc/configuration/AppConfig.java
@@ -2,7 +2,6 @@ package com.plantilla.springmvc.configuration;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 
 @Configuration
 @ComponentScan(basePackages = {"com.plantilla.springmvc.repository","com.plantilla.springmvc.service","com.plantilla.springmvc.configuration","org.springframework.security.core.userdetails.UserDetailsService"})
